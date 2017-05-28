c7c755ef8957173508c4637ee729e05c585e1349
Bogdan Orzea <bogdan.orzea@gmail.com>
2017-05-28 23:32:04 +0300

    Moves the Subject class inside the ReportCard class

--- app/src/main/java/com/bogdanorzea/reportcardapp/ReportCard.java
+++ app/src/main/java/com/bogdanorzea/reportcardapp/ReportCard.java
@@ -2,6 +2,7 @@
 package com.bogdanorzea.reportcardapp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ReportCard {
     private final String studentFirstName;
@@ -43,7 +44,7 @@ public class ReportCard {
     /**
      * Sets the school year
      *
-     * @param schoolYear
+     * @param schoolYear integer grader than 1970
      */
     public void setSchoolYear(int schoolYear) {
         if (this.schoolYear == -1) {
@@ -78,7 +79,7 @@ public class ReportCard {
     /**
      * Add uniquely named subject to ReportCard
      *
-     * @param sub
+     * @param sub Subject
      */
     public void addSubject(Subject sub) {
         if (!hasSubject(sub.getSubjectName())) {
@@ -125,15 +126,147 @@ public class ReportCard {
         ret.append("Last Name: ").append(getStudentLastName()).append('\n');
         ret.append("School Name: ").append(getSchoolName()).append('\n');
         ret.append("School Year: ").append(getSchoolYear()).append('\n');
-        ret.append("\n").append(Subject.PrintBorder());
-        ret.append("\n").append(Subject.PrintHeader());
-        ret.append("\n").append(Subject.PrintBorder());
+        ret.append("\n").append(Subject.printBorder());
+        ret.append("\n").append(Subject.printHeader());
+        ret.append("\n").append(Subject.printBorder());
 
         for (Subject subject : subjectList) {
             ret.append("\n").append(subject.prettyPrint());
         }
         ret.append("\n");
-        ret.append(Subject.PrintBorder());
+        ret.append(Subject.printBorder());
         return ret.toString();
+    }
+}
+
+class Subject {
+    private final static int MAX_GRADES = 4;
+
+    // Each subject has four grades ranging from 1 to 100, two for each semester.
+    private int gradeArray[] = new int[MAX_GRADES];
+
+    private String subjectName;
+
+    private Subject() {
+    }
+
+    /**
+     * Constructor for a Subject from the ReportCard providing the name
+     *
+     * @param name Subject name
+     */
+    Subject(String name) {
+        subjectName = name;
+    }
+
+    /**
+     * Constructor for a Subject from the ReportCard providing the name and the grades
+     *
+     * @param name   Subject name
+     * @param grades Coma separated integers or an array of integers representing grades
+     */
+    Subject(String name, int... grades) {
+        this(name);
+        for (int grade : grades) {
+            addGrade(grade);
+        }
+    }
+
+    static String printHeader() {
+        return String.format("|%1$-25s|%2$5s|%3$5s|%6$5s|%4$5s|%5$5s|%7$5s|%8$5s|",
+                "Subject Name",
+                "1st",
+                "2nd",
+                "3rd",
+                "4th",
+                "SEM 1",
+                "SEM 2",
+                "FNL");
+    }
+
+    static String printBorder() {
+        return new String(new char[69]).replace("\0", "-");
+    }
+
+    /**
+     * @return Return the subject name
+     */
+    String getSubjectName() {
+        return subjectName;
+    }
+
+    /**
+     * Add a grade to the Subject
+     *
+     * @param grade Integer between 1 and 100
+     */
+    void addGrade(int grade) {
+        if (grade < 1 || grade > 100) {
+            return;
+        }
+        for (int i = 0; i < MAX_GRADES; i++) {
+            if (gradeArray[i] < 1) {
+                gradeArray[i] = grade;
+                return;
+            }
+        }
+    }
+
+    /**
+     * @return Returns array of integers representing the Subject's grades
+     */
+    int[] getGrades() {
+        return gradeArray;
+    }
+
+    /**
+     * @return First semester average grade
+     */
+    int getFirstSemGrade() {
+        int i = 0;
+        int sum = 0;
+        for (; i < MAX_GRADES / 2; i++) {
+            sum += gradeArray[i];
+        }
+        return (int) (sum / (MAX_GRADES / 2.0) + 0.5);
+    }
+
+    /**
+     * @return First semester average grade
+     */
+    int getSecondSemGrade() {
+        int i = MAX_GRADES / 2;
+        int sum = 0;
+        for (; i < MAX_GRADES; i++) {
+            sum += gradeArray[i];
+        }
+        return (int) (sum / (MAX_GRADES / 2.0) + 0.5);
+    }
+
+    /**
+     * @return Subject final grade
+     */
+    int getFinalGrade() {
+        return (int) ((getFirstSemGrade() + getSecondSemGrade()) / (MAX_GRADES / 2.0) + 0.5);
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" +
+                "subjectName='" + subjectName + '\'' +
+                ", gradeArray=" + Arrays.toString(gradeArray) +
+                '}';
+    }
+
+    String prettyPrint() {
+        return String.format("|%1$-25s|%2$5d|%3$5d|%6$5d|%4$5d|%5$5d|%7$5d|%8$5d|",
+                subjectName,
+                gradeArray[0],
+                gradeArray[1],
+                gradeArray[2],
+                gradeArray[3],
+                getFirstSemGrade(),
+                getSecondSemGrade(),
+                getFinalGrade());
     }
 }
